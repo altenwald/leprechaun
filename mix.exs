b1085bfdc58b34a7563a5d0b24df8cd68348f3d0
Manuel Rubio <manuel@altenwald.com>
2022-06-29 13:35:54 +0200

    1.0.2 - refactoring Game, improve internal quality and add version for resources

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Leprechaun.MixProject do
   def project do
     [
       app: :leprechaun,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
